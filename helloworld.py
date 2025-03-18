7110cdd1c094fbc67df2f8df9f8a4316f4307659
Sam McGuire <45981861+SamJMcguire@users.noreply.github.com>
2025-03-18 14:14:40 +1300

    Update helloworld.py
    Rewrote whole code

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,30 @@
-# This is a comment
-print("Hello world")
-print("This is a new line")
+'''
+Rumplestilkson 
+author: Sam McGuire
+version: 1
+
+Description:  This is a guessing game based on the tale
+of Rumplestilskon.  Users have three chances to guess the 
+name randomly selected from the list
+
+'''
+
+# Import library or external code. OS for sleep and clear screen
+import os
+
+# Set variables for list, user guess, guess counter 
+
+INT_GUESSREMAINING = 3
+bool_endLoop = False
+str_userGuess = ''
+str_userName = ''
+
+# Creating game functions
+
+def greeting():
+    str_userName = input("Hello.  What is your name?")
+    print(f"Hello {str_userName}")
+    
+    
+# The Main program
+greeting()
